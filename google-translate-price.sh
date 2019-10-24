6ea14a5933f7f0bd17d0adc52410caf362c82cbd
Daniel Ran Lehmann <danielran11@gmail.com>
2019-10-23 22:08:19 -0500

    more accurate pricing (google-translate-price) by excluding already translated english files from the set considered.

--- google-translate-price.sh
+++ google-translate-price.sh
@@ -1,29 +1,38 @@
 pricingSource="https://cloud.google.com/translate/pricing"
 
-fileSum=0
+uniqueFileSum=0
+translatedFileSum=0
 charSum=0
 freeTierCharLimit=500000
 contactTierCharLimit=1000000000 # a billion
 
 for filename in ./data/09-25-2019/*/*/*.txt; do
-  charCount=($(wc -m < $filename))
-  charSum=$(($charSum + $charCount))
-  fileSum=$(($fileSum + 1))
+  if [[ $filename != */en.txt ]]; then
+    charCount=($(wc -m < $filename))
+    charSum=$(($charSum + $charCount))
+    translatedFileSum=$(($translatedFileSum + 1))
+  fi
+  uniqueFileSum=$(($uniqueFileSum + 1))
 done
 
+translatedFilesPct=`echo $translatedFileSum/$uniqueFileSum|bc -l`
+translatedFilesPct=`echo 1.0-$translatedFilesPct|bc -l`
+translatedFilesPct=`echo $translatedFilesPct*100.0|bc -l`
+
 echo "Data summary:"
-echo "\tTotal number of unique files:\t$fileSum"
 echo "\tTotal number of characters:\t$charSum"
+echo "\tTotal number of files:\t\t$translatedFileSum"
+printf "\tFiles translated:\t\t%.2f%%" $translatedFilesPct
 
 if [ "$charSum" -ge "$contactTierCharLimit" ]; then
   echo "Too many characters to translate. (Exceeding a billion character limit - $charSum).\bPlease contact Google developer team for resolution: $pricingSource"
 else
-  charPerMillion=`scale=2; echo $charSum/1000000|bc -l`
+  charPerMillion=`echo $charSum/1000000|bc -l`
 
-  price=`scale=2; echo 20*$charPerMillion|bc -l`
-  pbmtPrice=`scale=2; echo 20*$charPerMillion|bc -l`
-  nmtPrice=`scale=2; echo 20*$charPerMillion|bc -l`
-  autoMLPrice=`scale=2; echo 80*$charPerMillion|bc -l`
+  price=`echo 20*$charPerMillion|bc -l`
+  pbmtPrice=`echo 20*$charPerMillion|bc -l`
+  nmtPrice=`echo 20*$charPerMillion|bc -l`
+  autoMLPrice=`echo 80*$charPerMillion|bc -l`
 
   if [ "$charSum" -le "$freeTierCharLimit" ]; then
     nmtPrice=0.0
