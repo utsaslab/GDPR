3b368e7d521d55e70ca3e6c04598ff5952d88cbe
Daniel Ran Lehmann <danielran11@gmail.com>
2019-10-23 21:35:39 -0500

    google-translate-price shell script now supports floating point precision.

--- google-translate-price.sh
+++ google-translate-price.sh
@@ -1,8 +1,9 @@
-echo "Calculating price, please wait.."
 pricingSource="https://cloud.google.com/translate/pricing"
 
 fileSum=0
 charSum=0
+freeTierCharLimit=500000
+contactTierCharLimit=1000000000 # a billion
 
 for filename in ./data/09-25-2019/*/*/*.txt; do
   charCount=($(wc -m < $filename))
@@ -10,37 +11,35 @@ for filename in ./data/09-25-2019/*/*/*.txt; do
   fileSum=$(($fileSum + 1))
 done
 
-echo "\b"
 echo "Data summary:"
 echo "\tTotal number of unique files:\t$fileSum"
 echo "\tTotal number of characters:\t$charSum"
 
-freeTierCharLimit=500000
-contactTierCharLimit=1000000000 # a billion
-
 if [ "$charSum" -ge "$contactTierCharLimit" ]; then
   echo "Too many characters to translate. (Exceeding a billion character limit - $charSum).\bPlease contact Google developer team for resolution: $pricingSource"
 else
-  charPerMillion=$(python3 -c "from math import ceil; print(ceil($charSum/1000000.0))")
+  charPerMillion=`scale=2; echo $charSum/1000000|bc -l`
+
+  price=`scale=2; echo 20*$charPerMillion|bc -l`
+  pbmtPrice=`scale=2; echo 20*$charPerMillion|bc -l`
+  nmtPrice=`scale=2; echo 20*$charPerMillion|bc -l`
+  autoMLPrice=`scale=2; echo 80*$charPerMillion|bc -l`
+
+  if [ "$charSum" -le "$freeTierCharLimit" ]; then
+    nmtPrice=0.0
+    autoMLPrice=0.0
+  fi
 
   echo "\b"
-  echo "Estimated pricing:"
-  echo "\tFeature detection (API v2, v3):\t\$$((20 * $charPerMillion))"
-  echo "\tText translation (API v2):\t\$$((20 * $charPerMillion))"
-  echo "\tText translation (PBMT general models) (API v3):\t\$$((20 * $charPerMillion))"
+  echo "Price of feature (Text Translation):"
 
-  if [ "$charSum" -le "$freeTierCharLimit" ]; then
-    echo "\tText translation (NMT general models) (API v3):\tFree"
-  else
-    echo "\tText translation (NMT general models) (API v3):\t\$$((20 * $charPerMillion))"
-  fi
+  printf "\t(API v2):\t\t\t\$%.2f\n" $price
+  printf "\t(PBMT general models) (API v3):\t\$%.2f\n" $pbmtPrice
+  printf "\t(NMT general models) (API v3):\t\$%.2f\n" $nmtPrice
+  printf "\t(AutoML models) (API v3):\t\$%.2f\n" $autoMLPrice
 
-  if [ "$charSum" -le "$freeTierCharLimit" ]; then
-    echo "\tText translation (AutoML models) (API v3):\tFree"
-  else
-    echo "\tText translation (AutoML models) (API v3):\t\$$((80 * $charPerMillion))"
-  fi
   echo "\b\b"
-  echo "\tPricing equation: Price per million * Number of million chars"
+  echo "\tPricing equation: Price per million * Number of chars (per million)"
+  echo "\tPricing example: \$20 * (75.000 chars/10^6 chars)) = \$20*0.075 = \$1.5"
   echo "\tSource: $pricingSource"
 fi
